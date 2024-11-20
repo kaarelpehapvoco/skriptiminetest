61b03782e2f23bbd878daebd13d09147378448b3
Kaarel Pehap <kaarel.pehap@voco.ee>
2024-11-20 10:46:01 +0200

    Add files via upload

---
+++ pr4ii.ps1
@@ -0,0 +1,11 @@
+﻿$countries = @('India', 'Australia', 'China')
+$capitals = @('New Delhi', 'Canberra', 'Beijing')
+
+Write-Host "Select country:" -ForegroundColor Yellow
+for($i = 0; $i -lt $countries.Count; $i++)
+{
+Write-Host ($i+1)':'$countries[$i] -ForegroundColor Green
+}
+
+$selectedCountry = Read-Host "Please select country"
+Write-Host $countries[$selectedCountry-1]"'s capital is"$capitals[$selectedCountry-1] -ForegroundColor Green

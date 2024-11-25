3ba420dabf81ccc1e0be7c33c9b997f0e055e631
Kaarel Pehap <kaarel.pehap@voco.ee>
2024-11-25 13:27:58 +0200

    Add files via upload

---
+++ pr10.ps1
@@ -0,0 +1,17 @@
+﻿#start the notepad process 3 times
+
+for($i = 0; $i -lt 3; $i++){
+Start-Process "C:\Windows\notepad.exe"
+}
+
+$statementDisplayed = 0;
+
+do{
+Write-Host "Notepad is running"
+$statementDisplayed++
+Start-Sleep -Seconds 1
+}until(!(Get-Process | Where{$_.ProcessName -eq 'Notepad'}))
+
+#show the number of times, the statement was displayed
+
+Write-Host "The statement was displayed $statementDisplayed times"

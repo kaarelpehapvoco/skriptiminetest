c61814d8311485a3e1b8b1f869751bd0bfcd99c0
Kaarel Pehap <kaarel.pehap@voco.ee>
2024-11-25 12:08:51 +0200

    Add files via upload

---
+++ pr7.ps1
@@ -0,0 +1,10 @@
+﻿$groups = @('Red', 'Green', 'Yellow', 'Blue')
+$studentGroups = @()
+
+for($i = 1; $i -le 20; $i++)
+{
+    $group = $groups | Get-Random
+    $studentGroups += [PSCustomObject]@{'RollNumber'=$i; 'Group'=$group}
+}
+
+$studentGroups
